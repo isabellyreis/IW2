eec777e4644d145706abbfc96e996407c07fc5f4
isabellyreis <100379680+isabellyreis@users.noreply.github.com>
2023-03-08 16:55:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const http = require('http')
+//Rotas
+const express = require('express')
+const app = express()
 
-http.createServer(function(req,res){
-    res.end("Meu primeiro servidor Nodejs")
-}).listen(8081)
+app.get("/", function(req,res){
+    res.send("Minha primeira página no NodeJS")
+})
+app.get("/blog", function(req,res){ 
+    res.send("Minha página blog")   
+})
+app.get("/sobre", function(req,res){
+    res.send("Minha página sobre")
+})
 
-console.log("Servidor node funcionando!")
+app.listen(8081, function(req,res) {
+    console.log("servidor funcinando")
+
+})
